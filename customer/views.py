6935a0ed137a3c16ee5baffe27f4833d4d5fb989
JacquesIversen <jackiracki1@gmail.com>
2023-07-04 11:04:09 +0000

    post method added

--- customer/views.py
+++ customer/views.py
@@ -29,3 +29,38 @@ class Order(View):
 
         # render
         return render(request, 'customer/order.html', context)
+
+    def post(self, request, *args, **kwargs):
+        order_items = {
+            'items': []
+        }
+
+        items = request.POST.getlist('items[]')
+
+        for item in items:
+            kayak_variant = KayakVariant.objects.get(pk__contains=int(item))
+            item_data = {
+                'id': kayak_variant.pk,
+                'name': kayak_variant.name,
+                'price': kayak_variant.price
+            }
+
+            order_items['items'].append(item_data)
+
+            price = 0
+            item_ids = []
+
+            for item in order_items['items']:
+                price += item['price']
+                item.ids.append(item['id'])
+
+            order = OrderModel.objects.create(price=price)
+            order.items.add(*item_id)
+
+            context = {
+                'items': order_items['items'],
+                'price': price
+            }
+
+            return render(request, 'customer/order_confirmation.html', context)
+
